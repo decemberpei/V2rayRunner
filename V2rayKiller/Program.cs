58beeeb698b13a7c2f8c344306989cc51b7108de
decemberpei <decemberpei@gmail.com>
2021-06-16 21:05:58 +0800

    add v2raykiller to kill v2ray runner and v2ray instances

---
+++ V2rayKiller/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V2rayKiller
+{
+    class Program
+    {
+        private static string V2RAY = "v2ray";
+        private static string V2RAY_RUNNER = "V2rayRunner";
+
+        static void Main(string[] args)
+        {
+            foreach (var process in Process.GetProcessesByName(V2RAY))
+            {
+                if (process.Id != Process.GetCurrentProcess().Id)
+                {
+                    process.Kill();
+                }
+            }
+            foreach (var process in Process.GetProcessesByName(V2RAY_RUNNER))
+            {
+                if (process.Id != Process.GetCurrentProcess().Id)
+                {
+                    process.Kill();
+                }
+            }
+        }
+    }
+}
